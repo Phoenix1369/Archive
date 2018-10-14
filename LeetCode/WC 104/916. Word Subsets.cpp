a91eb5cf5524d6fcdb0116ef1cff5a1b021cc770
Patrick Au <Phoenix1369@users.noreply.github.com>
2018-10-13 22:34:54 -0400

    Add LC problem 916

---
+++ LeetCode/WC 104/916. Word Subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    const static int MAXA = 26;
+public:
+    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
+        int bnt[MAXA]{ };
+        for (const string& s: B) {
+            int cnt[MAXA]{ };
+            for (const char& c: s) {
+                ++cnt[c-'a'];
+            }
+            for (int i = 0; i < MAXA; ++i) {
+                bnt[i] = max(cnt[i], bnt[i]);
+            }
+        }
+        vector<string> ret;
+        for (const string& s: A) {
+            int cnt[MAXA]{ };
+            for (const char& c: s) {
+                ++cnt[c-'a'];
+            }
+            bool yes = true;
+            for (int i = 0; i < MAXA; ++i) {
+                yes &= (cnt[i] >= bnt[i]);
+            }
+            if (yes) ret.push_back(s);
+        }
+        return ret;
+    }
+};
