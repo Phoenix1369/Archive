19fc134dc6cfa981e525e7dfbaaf8db809049e98
Phoenix1369 <Phoenix1369@users.noreply.github.com>
2019-06-17 16:52:39 -0400

    Add FHC 2019 QR

---
+++ Facebook/2019/r0/15: Leapfrog: Ch. 1.cc
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstring>
+#define PROBLEM "leapfrog_ch_"
+const int MAXN = 5005;
+char lily[MAXN];
+int T;
+
+int main() {
+//    freopen(PROBLEM "_sample_input.txt", "r", stdin);
+    freopen(PROBLEM ".txt", "r", stdin);
+    freopen(PROBLEM ".out", "w", stdout);
+    scanf("%d", &T);
+    for (int t = 1; t <= T; ++t) {
+        scanf(" %s", lily);
+        int N = strlen(lily);
+        int beta = 0;
+        int free = 0;
+        for (int i = 1; i < N; ++i) {
+            if (lily[i] == 'B') {
+                ++beta;
+            } else {
+                ++free;
+            }
+        }
+		bool yes = (beta >= 1) && (free >= 1) && (beta >= free);
+        printf("Case #%d: %c\n", t, yes ? 'Y' : 'N');
+    }
+    return 0;
+}
