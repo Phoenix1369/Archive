2d74cfab9d3372a840e0759d5c62211c9a4423d0
Phoenix1369 <Phoenix1369@users.noreply.github.com>
2019-08-25 15:00:14 -0600

    Update LC problem 1163

--- LeetCode/Weekly/150/1163. Last Substring in Lexicographical Order.cc
+++ LeetCode/Weekly/150/1163. Last Substring in Lexicographical Order.cc
@@ -1,36 +1,31 @@
 const int MAXN = 4e5+5;
-bool live[MAXN];
+bool live[MAXN]{ };
 
 class Solution {
 public:
     string lastSubstring(string s) {
-        char Z = 'a';
-        for (char c: s) Z = max(c, Z);
-
         int N = s.size();
+        
         vector<int> good;
-        for (int i = 0; i < N; ++i) live[i] = false;
+        good.reserve(N);
         for (int i = 0; i < N; ++i) {
-            if (s[i] != Z) continue;
             good.push_back(i);
             live[i] = true;
         }
-
+        
         vector<int> succ;
-        for (int k = 1; good.size() > 1u; ++k) {
-            Z = 'a';
+        for (int k = 0; good.size() > 1u; ++k) {
+            char Z = 'a';
             for (auto p: good) {
-                if (p + k >= N) {
-                    live[p] = false;
-                    continue;
-                }
+                if (p + k >= N) continue;
+                Z = max(s[p + k], Z);
+                if (!k) continue;
                 if (live[p + k]) {
                     live[p + k] = false;
                 }
-                Z = max(s[p + k], Z);
             }
             for (auto p: good) {
-                if (s[p + k] != Z) live[p] = false;
+                if ((p + k >= N) || (s[p + k] != Z)) live[p] = false;
                 if (live[p]) succ.push_back(p);
             }
             good = move(succ);
